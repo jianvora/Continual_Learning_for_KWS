dc04ca2873407c9c6c1a546a4a52bb0357bc0878
anweshm136 <anweshm136@gmail.com>
2021-03-31 16:15:19 +0530

    small change in final

--- final.py
+++ final.py
@@ -2,7 +2,7 @@ from .Identification.data_io import read_conf
 from .Identification.speaker_id import main_func
 from .TCResnet.TCResnet_KWS import final_func
 
-options = read_conf()
+#options = read_conf()
 
 kws_softmax = final_func()
-spkid_softmax = main_func(options)
+spkid_softmax = main_func()
